fc03ab16fcfff1af07f8ebc0d19ec5113480e63a
Nic Cheneweth <nchenewe@thoughtworks.com>
2021-05-14 18:09:56 -0500

    [ncheneweth] initial test of new circleci dynamic configuration for pipelines

---
+++ scripts/vpc.test.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+export ENVIRONMENT=$1
+export AWS_ACCOUNT_ID=$(cat ${ENVIRONMENT}.auto.tfvars.json | jq -r .account_id)
+
+aws sts assume-role --output json --role-arn arn:aws:iam::${AWS_ACCOUNT_ID}:role/DPSTerraformRole --role-session-name awspec-test > credentials
+
+export AWS_ACCESS_KEY_ID=$(cat credentials | jq -r ".Credentials.AccessKeyId")
+export AWS_SECRET_ACCESS_KEY=$(cat credentials | jq -r ".Credentials.SecretAccessKey")
+export AWS_SESSION_TOKEN=$(cat credentials | jq -r ".Credentials.SessionToken")
+export AWS_DEFAULT_REGION=$(cat ${ENVIRONMENT}.auto.tfvars.json | jq -r .aws_region)
+
+rspec
+
+
+
