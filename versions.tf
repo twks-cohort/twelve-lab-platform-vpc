5544a824e3399f2e668f51d653bca71168fe5f92
lanzhao <lanzhao@thoughtworks.com>
2024-05-13 20:49:10 +0800

    add prefix twelve-

--- versions.tf
+++ versions.tf
@@ -19,14 +19,14 @@ provider "aws" {
   region  = var.aws_region
   assume_role {
     role_arn     = "arn:aws:iam::${var.account_id}:role/${var.assume_role}"
-    session_name = "lab-platform-vpc-${var.cluster_name}"
+    session_name = "twelve-lab-platform-vpc-${var.cluster_name}"
   }
 
   default_tags {
     tags = {
       env      = var.cluster_name
       cluster  = var.cluster_name
-      pipeline = "lab-platform-vpc"
+      pipeline = "twelve-lab-platform-vpc"
       "kubernetes.io/cluster/${var.cluster_name}" = "shared"
     }
   }
